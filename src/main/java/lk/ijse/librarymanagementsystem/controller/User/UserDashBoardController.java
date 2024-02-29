49cd024bb54f85ba16bf6ac7aada16773bc64d19
Amodhx <amxdhnanditha@gmail.com>
2024-03-01 02:13:03 +0530

    bookOBJ form done

--- src/main/java/lk/ijse/librarymanagementsystem/controller/User/UserDashBoardController.java
+++ src/main/java/lk/ijse/librarymanagementsystem/controller/User/UserDashBoardController.java
@@ -1,4 +1,67 @@
 package lk.ijse.librarymanagementsystem.controller.User;
 
-public class UserDashBoardController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lombok.SneakyThrows;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserDashBoardController implements Initializable {
+
+    @FXML
+    private AnchorPane ancpane;
+
+    @FXML
+    private AnchorPane booksPane;
+
+    @FXML
+    private AnchorPane transactionpane;
+
+    @FXML
+    void onBookManageCLick(ActionEvent event) {
+
+    }
+    @FXML
+    void ontransactionManageClick(ActionEvent event) {
+
+    }
+
+    @SneakyThrows
+    @FXML
+    void onlogoutClick(ActionEvent event) {
+        Stage stage = (Stage) booksPane.getScene().getWindow();
+        stage.close();
+        Stage stage1 = new Stage();
+        stage1.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/loginpage.fxml"))));
+        stage1.setResizable(false);
+        stage1.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        navigateUI("/view/user/userMainform.fxml");
+        btnCLR(booksPane);
+    }
+
+    private void btnCLR(AnchorPane pane) {
+        removeClr();
+        pane.setStyle("-fx-background-color: black");
+    }
+
+    private void removeClr() {
+        booksPane.setStyle("-fx-background-color: white");
+        transactionpane.setStyle("-fx-background-color: white");
+    }
+
+    @SneakyThrows
+    private void navigateUI(String url) {
+        ancpane.getChildren().clear();
+        ancpane.getChildren().add(FXMLLoader.load(getClass().getResource(url)));
+    }
 }
