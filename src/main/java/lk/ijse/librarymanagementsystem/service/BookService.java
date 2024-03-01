7e4ee92c63ae2b65517d5e6a1c2a40e86598929f
Amodhx <amxdhnanditha@gmail.com>
2024-03-01 19:46:17 +0530

    user transaction done

--- src/main/java/lk/ijse/librarymanagementsystem/service/BookService.java
+++ src/main/java/lk/ijse/librarymanagementsystem/service/BookService.java
@@ -10,12 +10,26 @@ import java.util.List;
 public class BookService {
 
     BookDAOImpl bookDAO = new BookDAOImpl();
+    public Book getBookUsingID(int id){
+        return bookDAO.getBookByID(id);
+    }
+    public boolean updateBookStatus(int id ,String status){
+        return bookDAO.updateBook(id,status);
+    }
     public boolean saveBook(BookDTO bookDTO){
         return bookDAO.saveBook(new Book(bookDTO.getId(), bookDTO.getTitle(), bookDTO.getAuthor(),
                 bookDTO.getGenre(), bookDTO.getStatus(), bookDTO.getUrl()));
     }
     public ArrayList<BookDTO> getAllBooks(){
         List<Book> allBooks = bookDAO.getAllBooks();
+        ArrayList<BookDTO> arrayList = new ArrayList<>();
+        for (Book b : allBooks){
+            arrayList.add(new BookDTO(b.getId(),b.getTitle(),b.getAuthor(),b.getGenre(),b.getStatus(),b.getUrl()));
+        }
+        return arrayList;
+    }
+    public ArrayList<BookDTO> getBooksFilterWithGenre(String genre){
+        List<Book> allBooks = bookDAO.getBooksFilterWithGenre(genre);
         ArrayList<BookDTO> arrayList = new ArrayList<>();
         for (Book b : allBooks){
             arrayList.add(new BookDTO(b.getId(),b.getTitle(),b.getAuthor(),b.getGenre(),b.getStatus(),b.getUrl()));
