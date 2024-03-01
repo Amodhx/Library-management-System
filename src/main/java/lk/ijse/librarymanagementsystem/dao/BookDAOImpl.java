503e3c317b2c36aff2774ffc43dd8d5a3bc134ca
Amodhx <amxdhnanditha@gmail.com>
2024-03-02 00:12:46 +0530

    user transaction done

--- src/main/java/lk/ijse/librarymanagementsystem/dao/BookDAOImpl.java
+++ src/main/java/lk/ijse/librarymanagementsystem/dao/BookDAOImpl.java
@@ -96,4 +96,38 @@ public class BookDAOImpl {
         }
         return book;
     }
+
+    public boolean deleteBook(String id) {
+        Session session = null;
+        int x = 0 ;
+        try {
+            session = FactoryConfiguration.getFactoryConfiguration().getSession();
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createQuery("delete Book where id = :ID");
+            query.setParameter("ID",id);
+            x = query.executeUpdate();
+            transaction.commit();
+        }catch (Exception e){
+            new Alert(Alert.AlertType.ERROR).show();
+        }finally {
+            session.close();
+        }
+        return x > 0 ;
+    }
+
+    public boolean Update(Book book) {
+        Session session = null;
+        int x = 0 ;
+        try {
+            session = FactoryConfiguration.getFactoryConfiguration().getSession();
+            Transaction transaction = session.beginTransaction();
+            session.saveOrUpdate(book);
+            transaction.commit();
+        }catch (Exception e){
+            new Alert(Alert.AlertType.ERROR).show();
+        }finally {
+            session.close();
+        }
+        return true;
+    }
 }
