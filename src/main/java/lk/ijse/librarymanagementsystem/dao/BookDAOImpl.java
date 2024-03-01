e895ff0950466c0602abc4ee89ba58ce307c1c46
Amodhx <amxdhnanditha@gmail.com>
2024-03-01 19:45:48 +0530

    user transaction done

--- src/main/java/lk/ijse/librarymanagementsystem/dao/BookDAOImpl.java
+++ src/main/java/lk/ijse/librarymanagementsystem/dao/BookDAOImpl.java
@@ -5,6 +5,7 @@ import lk.ijse.librarymanagementsystem.config.FactoryConfiguration;
 import lk.ijse.librarymanagementsystem.entity.Book;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,5 +38,62 @@ public class BookDAOImpl {
             session.close();
         }
         return resultList;
+    }
+
+    public List<Book> getBooksFilterWithGenre(String genre) {
+        Session session = null;
+        List<Book> list = null;
+        try {
+            session = FactoryConfiguration.getFactoryConfiguration().getSession();
+            Query query = session.createQuery("from Book where genre = :GENRE");
+            query.setParameter("GENRE",genre);
+            list = query.getResultList();
+        }catch (Exception e){
+            new Alert(Alert.AlertType.ERROR).show();
+        }finally {
+            session.close();
+        }
+        return list;
+    }
+
+    public boolean updateBook(int id , String status) {
+        Session session = null;
+        int x = 0 ;
+        try {
+            session =FactoryConfiguration.getFactoryConfiguration().getSession();
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createQuery("update Book set status = :Sta where id = :ID");
+            query.setParameter("Sta",status);
+            query.setParameter("ID",id);
+            x = query.executeUpdate();
+            transaction.commit();
+        }catch (Exception e){
+            new Alert(Alert.AlertType.ERROR).show();
+        }finally {
+            session.close();
+        }
+        return x > 0 ;
+    }
+
+    public Book getBookByID(int id) {
+        Session session = null;
+        Book book = new Book();
+        try {
+            session = FactoryConfiguration.getFactoryConfiguration().getSession();
+            Query query = session.createQuery("from Book  where id = :ID");
+            query.setParameter("ID", id);
+            List<Book> resultList = query.getResultList();
+            book.setId(resultList.get(0).getId());
+            book.setTitle(resultList.get(0).getTitle());
+            book.setAuthor(resultList.get(0).getAuthor());
+            book.setGenre(resultList.get(0).getGenre());
+            book.setStatus(resultList.get(0).getStatus());
+            book.setUrl(resultList.get(0).getUrl());
+        }catch (Exception e){
+
+        }finally {
+            session.close();
+        }
+        return book;
     }
 }
