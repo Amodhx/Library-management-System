a1f0843a9d520d384c2c9f5b43a77131f37d84df
Amodhx <amxdhnanditha@gmail.com>
2024-03-01 19:47:22 +0530

    user transaction done

---
+++ src/main/java/lk/ijse/librarymanagementsystem/service/BorrowingDetailsService.java
@@ -0,0 +1,25 @@
+package lk.ijse.librarymanagementsystem.service;
+
+import lk.ijse.librarymanagementsystem.dao.BorrowingDAOImpl;
+import lk.ijse.librarymanagementsystem.dto.BorrowingDetailDTO;
+import lk.ijse.librarymanagementsystem.entity.Book;
+import lk.ijse.librarymanagementsystem.entity.BorrowingDetails;
+import lk.ijse.librarymanagementsystem.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BorrowingDetailsService {
+    BorrowingDAOImpl borrowingDAO = new BorrowingDAOImpl();
+    BookService bookService = new BookService();
+    LogginService logginService = new LogginService();
+    public List<BorrowingDetails> getDetails(int id){
+        List<BorrowingDetails> list = borrowingDAO.getDetailsBYid(id);
+       return list;
+    }
+    public boolean saveBorrowingDetails(BorrowingDetailDTO borrowingDetailDTO){
+        Book bookUsingID = bookService.getBookUsingID(borrowingDetailDTO.getBookID());
+        User userById = logginService.getUserById(borrowingDetailDTO.getUserID());
+        return borrowingDAO.saveBorrowingDetail(new BorrowingDetails(borrowingDetailDTO.getId(),borrowingDetailDTO.getBorrowingDate(),borrowingDetailDTO.getDueDate(),userById,bookUsingID));
+    }
+}
