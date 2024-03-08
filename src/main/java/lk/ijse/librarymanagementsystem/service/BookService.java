44416434f4312d783913b871489c044d07bf46fd
Amodhx <amxdhnanditha@gmail.com>
2024-03-08 09:45:44 +0530

    book service done

--- src/main/java/lk/ijse/librarymanagementsystem/service/BookService.java
+++ src/main/java/lk/ijse/librarymanagementsystem/service/BookService.java
@@ -1,47 +1,20 @@
 package lk.ijse.librarymanagementsystem.service;
 
-import lk.ijse.librarymanagementsystem.dao.BookDAOImpl;
 import lk.ijse.librarymanagementsystem.dto.BookDTO;
 import lk.ijse.librarymanagementsystem.entity.Book;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class BookService {
+public interface BookService extends AllService{
+     Book getBookUsingID(int id);
+     boolean updateBookStatus(int id ,String status);
 
-    BookDAOImpl bookDAO = new BookDAOImpl();
-    public Book getBookUsingID(int id){
-        return bookDAO.getBookByID(id);
-    }
-    public boolean updateBookStatus(int id ,String status){
-        return bookDAO.updateBook(id,status);
-    }
-    public boolean saveBook(BookDTO bookDTO){
-        return bookDAO.saveBook(new Book(bookDTO.getId(), bookDTO.getTitle(), bookDTO.getAuthor(),
-                bookDTO.getGenre(), bookDTO.getStatus(), bookDTO.getUrl(),new ArrayList<>()));
-    }
-    public ArrayList<BookDTO> getAllBooks(){
-        List<Book> allBooks = bookDAO.getAllBooks();
-        ArrayList<BookDTO> arrayList = new ArrayList<>();
-        for (Book b : allBooks){
-            arrayList.add(new BookDTO(b.getId(),b.getTitle(),b.getAuthor(),b.getGenre(),b.getStatus(),b.getUrl()));
-        }
-        return arrayList;
-    }
-    public ArrayList<BookDTO> getBooksFilterWithGenre(String genre){
-        List<Book> allBooks = bookDAO.getBooksFilterWithGenre(genre);
-        ArrayList<BookDTO> arrayList = new ArrayList<>();
-        for (Book b : allBooks){
-            arrayList.add(new BookDTO(b.getId(),b.getTitle(),b.getAuthor(),b.getGenre(),b.getStatus(),b.getUrl()));
-        }
-        return arrayList;
-    }
+     boolean saveBook(BookDTO bookDTO);
+     ArrayList<BookDTO> getAllBooks();
+     ArrayList<BookDTO> getBooksFilterWithGenre(String genre);
 
-    public boolean deleteBook(String id) {
-        return bookDAO.deleteBook(id);
-    }
+     boolean deleteBook(String id);
 
-    public boolean updateBook(BookDTO bookDTO) {
-        return bookDAO.Update(new Book(bookDTO.getId(),bookDTO.getTitle(),bookDTO.getAuthor(),bookDTO.getGenre(),bookDTO.getStatus(),bookDTO.getUrl(),new ArrayList<>()));
-    }
+     boolean updateBook(BookDTO bookDTO);
 }
