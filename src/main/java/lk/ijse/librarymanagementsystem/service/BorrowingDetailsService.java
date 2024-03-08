d3eabc6e295566a9544896a78c9f3ad80d1bb3d9
Amodhx <amxdhnanditha@gmail.com>
2024-03-08 09:47:05 +0530

    shop service done

--- src/main/java/lk/ijse/librarymanagementsystem/service/BorrowingDetailsService.java
+++ src/main/java/lk/ijse/librarymanagementsystem/service/BorrowingDetailsService.java
@@ -1,33 +1,16 @@
 package lk.ijse.librarymanagementsystem.service;
 
-import lk.ijse.librarymanagementsystem.dao.BorrowingDAOImpl;
 import lk.ijse.librarymanagementsystem.dto.BorrowingDetailDTO;
 import lk.ijse.librarymanagementsystem.entity.Book;
 import lk.ijse.librarymanagementsystem.entity.BorrowingDetails;
 import lk.ijse.librarymanagementsystem.entity.User;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class BorrowingDetailsService {
-    BorrowingDAOImpl borrowingDAO = new BorrowingDAOImpl();
-    BookService bookService = new BookService();
-    LogginService logginService = new LogginService();
-    public boolean updateDueTransaction(int id){
-        return borrowingDAO.updateDueDate(id);
-    }
-    public List<BorrowingDetails> getDetails(int id){
-        List<BorrowingDetails> list = borrowingDAO.getDetailsBYid(id);
-       return list;
-    }
-    public boolean saveBorrowingDetails(BorrowingDetailDTO borrowingDetailDTO){
-        Book bookUsingID = bookService.getBookUsingID(borrowingDetailDTO.getBookID());
-        User userById = logginService.getUserById(borrowingDetailDTO.getUserID());
-        return borrowingDAO.saveBorrowingDetail(new BorrowingDetails(borrowingDetailDTO.getId(),borrowingDetailDTO.getBorrowingDate(),borrowingDetailDTO.getDueDate(),borrowingDetailDTO.getStatus(),userById,bookUsingID));
-    }
+public interface BorrowingDetailsService extends AllService {
+     boolean updateDueTransaction(int id);
+     List<BorrowingDetails> getDetails(int id);
+     boolean saveBorrowingDetails(BorrowingDetailDTO borrowingDetailDTO);
 
-    public boolean updateStatus(int id) {
-
-        return borrowingDAO.updateStatus(id);
-    }
+     boolean updateStatus(int id);
 }
