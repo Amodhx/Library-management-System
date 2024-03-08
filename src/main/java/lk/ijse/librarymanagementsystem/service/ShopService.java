273e8cd1646dc42c9030c6415c104e31786805c5
Amodhx <amxdhnanditha@gmail.com>
2024-03-08 09:45:22 +0530

    shop service done

--- src/main/java/lk/ijse/librarymanagementsystem/service/ShopService.java
+++ src/main/java/lk/ijse/librarymanagementsystem/service/ShopService.java
@@ -1,27 +1,13 @@
 package lk.ijse.librarymanagementsystem.service;
 
-import lk.ijse.librarymanagementsystem.dao.ShopDAOImpl;
 import lk.ijse.librarymanagementsystem.dto.ShopDTO;
 import lk.ijse.librarymanagementsystem.entity.Branches;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ShopService {
-
-    ShopDAOImpl shopDAO = new ShopDAOImpl();
-    public boolean deleteShop(int id){
-        return shopDAO.deleteShop(id);
-    }
-    public ArrayList<ShopDTO> getAllShops(){
-        List<Branches> allShops = shopDAO.getAllShops();
-        ArrayList<ShopDTO> arrayList = new ArrayList<>();
-        for (Branches b : allShops){
-            arrayList.add(new ShopDTO(b.getId(),b.getName(),b.getCity(),b.getAddress(),b.getPostalCode()));
-        }
-        return arrayList;
-    }
-    public boolean saveShop(ShopDTO shopDTO){
-        return shopDAO.saveShop(new Branches(shopDTO.getId(), shopDTO.getName(), shopDTO.getCity(), shopDTO.getAddress(), shopDTO.getPostalCode()));
-    }
+public interface ShopService extends AllService{
+     boolean deleteShop(int id);
+     ArrayList<ShopDTO> getAllShops();
+     boolean saveShop(ShopDTO shopDTO);
 }
