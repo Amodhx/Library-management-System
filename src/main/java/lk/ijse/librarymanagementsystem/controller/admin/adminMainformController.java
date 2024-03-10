678f4d8b49d28750f1c7ecd79e7abc3ba5fa2d44
Amodhx <amxdhnanditha@gmail.com>
2024-03-10 07:50:24 +0530

    ads

--- src/main/java/lk/ijse/librarymanagementsystem/controller/admin/adminMainformController.java
+++ src/main/java/lk/ijse/librarymanagementsystem/controller/admin/adminMainformController.java
@@ -2,13 +2,22 @@ package lk.ijse.librarymanagementsystem.controller.admin;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import lk.ijse.librarymanagementsystem.dao.BookDAO;
+import lk.ijse.librarymanagementsystem.service.BookService;
+import lk.ijse.librarymanagementsystem.service.LogginService;
+import lk.ijse.librarymanagementsystem.service.ServiceFactory;
+import lk.ijse.librarymanagementsystem.service.ShopService;
 import lombok.SneakyThrows;
 
-public class adminMainformController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class adminMainformController implements Initializable {
 
     @FXML
     private Label totalbooklbl;
@@ -18,7 +27,11 @@ public class adminMainformController {
 
     @FXML
     private Label totaluserlbl;
+    BookService bookService = (BookService) ServiceFactory.getServiceFactory().getService(ServiceFactory.ServiceTypes.BOOKService);
 
+    LogginService logginService = (LogginService) ServiceFactory.getServiceFactory().getService(ServiceFactory.ServiceTypes.LOGGINService);
+
+    ShopService service = (ShopService) ServiceFactory.getServiceFactory().getService(ServiceFactory.ServiceTypes.SHOPService);
     @SneakyThrows
     @FXML
     void onAddAdminClick(MouseEvent event) {
@@ -28,4 +41,10 @@ public class adminMainformController {
         stage.show();
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        totalbooklbl.setText(String.valueOf(bookService.getBookCount()));
+        totaluserlbl.setText(String.valueOf(logginService.getUserCount()));
+        totalshoplbl.setText(String.valueOf(service.getShopCount()));
+    }
 }
