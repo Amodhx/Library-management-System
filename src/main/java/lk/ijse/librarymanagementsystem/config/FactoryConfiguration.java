8f4f08ca1f01a617bdb340f6dd68f1271dd16d8d
Amodhx <amxdhnanditha@gmail.com>
2024-03-18 12:10:04 +0530

    property config

--- src/main/java/lk/ijse/librarymanagementsystem/config/FactoryConfiguration.java
+++ src/main/java/lk/ijse/librarymanagementsystem/config/FactoryConfiguration.java
@@ -3,22 +3,33 @@ package lk.ijse.librarymanagementsystem.config;
 import lk.ijse.librarymanagementsystem.entity.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class FactoryConfiguration {
     private static FactoryConfiguration factoryConfiguration;
     private SessionFactory sessionFactory;
 
     private FactoryConfiguration() {
-        Configuration configuration = new Configuration().configure()
-                .addAnnotatedClass(Admin.class).addAnnotatedClass(User.class).addAnnotatedClass(Book.class).addAnnotatedClass(BorrowingDetails.class).addAnnotatedClass(Branches.class);
-        sessionFactory = configuration.buildSessionFactory();
-    }
-     public static FactoryConfiguration getFactoryConfiguration(){
-         return factoryConfiguration==null ? factoryConfiguration=new FactoryConfiguration() : factoryConfiguration;
 
-     }
-     public Session getSession(){
+        StandardServiceRegistryBuilder standardServiceRegistryBuilder = new StandardServiceRegistryBuilder();
+        standardServiceRegistryBuilder.loadProperties("hibernate.properties");
+
+        MetadataSources metadataSources = new MetadataSources(standardServiceRegistryBuilder.build());
+
+        metadataSources.addAnnotatedClass(User.class).addAnnotatedClass(Book.class).addAnnotatedClass(Admin.class).addAnnotatedClass(Branches.class).addAnnotatedClass(BorrowingDetails.class);
+
+        Metadata metadata = metadataSources.getMetadataBuilder().build();
+        sessionFactory = metadata.getSessionFactoryBuilder().build();
+    }
+
+    public static FactoryConfiguration getFactoryConfiguration(){
+        return (factoryConfiguration == null)?factoryConfiguration = new FactoryConfiguration() : factoryConfiguration;
+    }
+
+    public Session getSession(){
         return sessionFactory.openSession();
-     }
+    }
 }
